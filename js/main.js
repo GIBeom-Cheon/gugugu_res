eef7c90ef9d01401c49dcec245f86cc2b7c1e6b1
GIBeom-Cheon <110073263+GIBeom-Cheon@users.noreply.github.com>
2022-09-22 12:11:21 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// const ham = document.querySelector(".trigger");
+// function clickE() {
+//   ham.classList.toggle("on");
+// }
+
+// ham.addEventListener("click", clickE);
+
+// function click(ham) {
+//   ham.classList.toggle("on");
+// }
+
+$(".m_menuBtn").click(function () {
+  $(".menu_container").toggleClass("on");
+  $("svg text").toggleClass("stroke");
+  $(".m_menuBtn .line1").toggleClass("close");
+  $(".m_menuBtn .line2").toggleClass("close");
+  $(".m_menuBtn .line3").toggleClass("close");
+  $(".m_menuBtn .line4").toggleClass("close");
+});
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 820) {
+    $(".m_menuBtn span").addClass("scroll");
+    $(".header").addClass("linecolor");
+  } else {
+    $(".m_menuBtn span").removeClass("scroll");
+    $(".header").removeClass("linecolor");
+  }
+});
+
+console.log($(window).scrollTop());
